78a4ba938ebca502574c8d30164af0346cd31cc8
wilypoli <willijair20@gmail.com>
2024-10-29 12:17:06 -0500

    cambios en los intervalos

--- Biseccion.py
+++ Biseccion.py
@@ -4,8 +4,8 @@ import numpy as np
 def f(x):
     return x**3 - 7 * x**2 + 14 * x - 6
 
-# Método de bisección para encontrar una raíz de f(x) en el intervalo [a, b] con precisión de 10^-10
-def metodo_biseccion(f, a, b, tol=1e-10):
+# Método de bisección para encontrar una raíz de f(x) en el intervalo [a, b] con precisión de 10^-2
+def metodo_biseccion(f, a, b, tol=1e-2):
     # Verificamos que haya un cambio de signo en los extremos del intervalo
     if f(a) * f(b) >= 0:
         return None  # No garantiza que haya una raíz en el intervalo
@@ -32,7 +32,7 @@ def metodo_biseccion(f, a, b, tol=1e-10):
 
     return c, iter_count  # Retornamos la raíz aproximada y el número de iteraciones
 
-# Aplicamos el método en el intervalo [0, 1]
-raiz, iteraciones = metodo_biseccion(f, 0, 1)
+# Aplicamos el método en el intervalo [1, 3.2]
+raiz, iteraciones = metodo_biseccion(f, 1,3.2)
 print(raiz, iteraciones)
-print(f"La raiz aproximada con el metodo de biseccion con una presicion de 10*-10 es:{raiz}, ademas se lo hizo en {iteraciones} iteraciones")
+print(f"La raiz aproximada con el metodo de biseccion con una presicion de 10*-2 es:{raiz}, ademas se lo hizo en {iteraciones} iteraciones")
